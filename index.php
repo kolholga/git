9ae2e6b16f84e103697f8d097b9118fec0e31683
Olga <test@example.com>
2020-09-14 19:22:01 +0300

    add title

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<?php
+
+$title = 'my page';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title><?=$title;?></title>
 </head>
 <body>
     <h1>Title</h1>
